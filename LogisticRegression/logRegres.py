fa53068a032d7090c0f987158ff4b6f8b0a57f5e
lmlyzxiaop <245785811@qq.com>
2017-08-16 20:48:26 +0800

    another Set

--- LogisticRegression/logRegres.py
+++ LogisticRegression/logRegres.py
@@ -47,7 +47,7 @@ def stocGradAscent1(dataMatIn,classLabels,numIter=150):
 	for j in range(numIter):
 		dataIndex = range(m)
 		for i in range(m):
-			alpha = 4/(1.0+i+j) + 0.01
+			alpha = 4/(1.0+i+j) + 0.0001
 			randIndex = int(random.uniform(0,len(dataIndex)))
 			h = sigmoid(sum(dataMatIn[randIndex]*weights))
 			error = classLabels[randIndex] - h
@@ -96,7 +96,41 @@ def plotXi(count):
 	plt.ylabel('X2')
 	plt.show()
 
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0.5: return 1.0
+    else: return 0.0
 
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt'); frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr =[]
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights,cnt = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr =[]
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights))!= int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount)/numTestVec)
+    print "the error rate of this test is: %f" % errorRate
+    return errorRate
+
+def multiTest():
+    numTests = 10; errorSum=0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
 
 
 if __name__ == '__main__':
@@ -106,6 +140,7 @@ if __name__ == '__main__':
 	weights,count= stocGradAscent1(array(dataArr),labelMat)
 	plotBestFit(weights,filename)
 	plotXi(count)
+	multiTest()
 
 
 
